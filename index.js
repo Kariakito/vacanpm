5897805c95e9331504f3457a60e90ab558b02c73
kariakito <pilarmaestre@gmail.com>
2021-06-20 14:39:07 +0200

    the cow talks now

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const info = require('./information.js');
 
 console.log(info);
+
+var cowsay = require ("cowsay");
+
+const person = { name:"Pilar"};
+
+console.log(info);
+
+console.log(cowsay.say({
+    text : "Hello I'm" + " " + info.name + " " + "from" + " " + info.campus + " " + "Campus!"  ,
+    e : "oO",
+    T : "U "
+}));
+
